0647d4ae623b18dcd7e3e050d9c10594b97f4d16
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2022-06-13 20:41:27 +0300

    script

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,14 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector(".button-mob"),
-      closeMenuBtn: document.querySelector(".button-mob-close"),
-      menu: document.querySelector(".mob-disp-menu"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden");
-    }
-  })();
-  
+  const refs = {
+    openMenuBtn: document.querySelector(".button-mob"),
+    closeMenuBtn: document.querySelector(".mob-close-btn"),
+    menu: document.querySelector(".mob-disp-menu"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
