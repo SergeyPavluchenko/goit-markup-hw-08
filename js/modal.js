7dca119daf4e034dab9330b2b721967f25ccde0f
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2022-06-19 12:38:04 +0300

    modal

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[modal-open-window]"),
+    closeModalBtn: document.querySelector("[modal-close-window]"),
+    modal: document.querySelector("[modal-window]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
